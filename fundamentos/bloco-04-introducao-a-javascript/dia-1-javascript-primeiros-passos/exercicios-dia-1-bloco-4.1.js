e4b7696509e990d17e1eae750db59a6ef5eca571
Lucas Pinho <lucasffpinho@gmail.com>
2022-06-21 19:44:37 -0300

    condensado exercicios no file exercicios.js

--- fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios-dia-1-bloco-4.1.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios-dia-1-bloco-4.1.js
@@ -33,11 +33,49 @@ if (a > b && c) {
 }
 
 // exercício 4
+const numero = 1;
 
-
+if (numero > 0) {
+    console.log("positive");
+} else if (numero < 0) {
+    console.log("negative");
+} else {
+    console.log("zero");
+}
 
 // exercício 5
+const a = 80;
+const b = 70;
+const c = 30;
+
+if (a + b + c == 180) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
 // exercício 6
+function movimentosPecasDeXadrez() = ["rei", "dama", "torre", "bispo", "cavalo", "peão"]
+
+if(rei){
+    console.log("uma casa em qualquer direção");
+}else if(dama){
+    console.log("casas ilimitadas em qualquer direção");
+}else if(torre){
+    console.log("casas ilimitadas vertical ou horizontalmente");
+}else if(bispo){
+    console.log("casas ilimitadas nas diagonais");
+}else if(cavalo){
+    console.log("três casas em L");
+}else (peão){
+    console.log("uma casa verticalmente");
+}
+
+return(rei)
+
+
+
+
 // exercício 7
 // exercício 8
 // exercício 9
