47d40441dd17406e0987dc904a535c01b3640361
Lucas Pinho <lucasffpinho@gmail.com>
2022-06-21 18:57:57 -0300

    exercicio 3 resolvido

---
+++ fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios-dia-1-bloco-4.1.js
@@ -0,0 +1,49 @@
+// exercício 1
+const a = 1;
+
+const b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+// exercício 2
+const a = 49;
+const b = 50;
+
+if (a > b) {
+    console.log(a);
+} else {
+    console.log(b);
+}
+
+// exercício 3
+const a = 1;
+const b = 2;
+const c = 3;
+
+if (a > b && c) {
+    console.log(a);
+} else if (b > a && c) {
+    console.log(b);
+} else if (c > a && b) {
+    console.log(c);
+}
+
+// exercício 4
+
+
+
+// exercício 5
+// exercício 6
+// exercício 7
+// exercício 8
+// exercício 9
+// exercício 10
+// exercício 11
+// exercício 12
+// exercício 13
+// exercício 14
+// exercício 15
