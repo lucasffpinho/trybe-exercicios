4ffd8881c56438662348a3a255d03665b2f6b2ca
Lucas Pinho <lucasffpinho@gmail.com>
2022-06-23 16:20:06 -0300

    exercicios tipos primitivos, tipagem dinâmica e operações aritméticas

---
+++ fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios-tipos-primitivos-tipagem-dinamica-operacoes-aritmeticas.js
@@ -0,0 +1,16 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+
+const height = [5, 8];
+const area = 5 * 8;
+console.log(area);
+
+const perimeter = (height[0] + height[1]) * 2
+console.log(perimeter);
