188099685dca34003286fa5d9de5729ce10febee
Lucas Pinho <lucasffpinho@gmail.com>
2022-06-28 15:23:33 -0300

    exercícios do bloco-05 dia-01

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/exercicio-seletores-de-elementos.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText = "Top Gun";
+document.getElementById("second-paragraph").innerText = "The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis).";
+document.getElementById("subtitle").innerText = "Sinopse";
