5990bdb52b6fbda595b8fd9bce373fd5cb50c73f
Lucas Pinho <lucasffpinho@gmail.com>
2022-06-28 19:39:00 -0300

    exercícios bloco-05 dia-01 parte II

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/exercicio-seletores-de-elementos-2.js
@@ -0,0 +1,10 @@
+// Adicione uma classe igual para os dois parágrafos 
+// resolvido no .html
+
+// Recupere os seus parágrafos via código JavaScript, usando a função getElementsByClassName;
+document.getElementsByClassName('style-paragraph');
+// Altere algum estilo do primeiro deles.
+document.getElementsByClassName('style-paragraph')[0].style.backgroundColor = 'red';
+
+// Recupere o subtítulo e altere a cor dele usando a função getElementsByTagName.
+document.getElementsByTagName('h4')[0].style.color = 'blue';
--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/exercicio-seletores-de-elementos.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/exercicio-seletores-de-elementos.js
@@ -1,3 +1,8 @@
+// Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
 document.getElementById("page-title").innerText = "Top Gun";
+
+// Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
 document.getElementById("second-paragraph").innerText = "The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis).";
+
+// Por fim, recupere o subtítulo e altere-o também.
 document.getElementById("subtitle").innerText = "Sinopse";
