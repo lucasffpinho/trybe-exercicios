70e5ad74f938f9bd89b34000b015344adcc89886
Lucas Pinho <lucasffpinho@gmail.com>
2022-07-27 16:06:10 -0300

    Exercício do bloco 8.5 - Object Destructuring

---
+++ fundamentos/bloco-08-introcucao-a-javascript-es6-e-testes-unitarios/dia-5-JavaScript-ES6-spread-operator-parametro-rest-destructuring-e-mais/exercicioObjectDestructuring.js
@@ -0,0 +1,25 @@
+
+const daysOfWeek = {
+    workDays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+    weekend: ['Saturday', 'Sunday'],
+};
+
+const { workDays, weekend } = daysOfWeek;
+console.log(workDays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+console.log(weekend); // ['Saturday', 'Sunday']
+
+const weekdays = [...workDays, ...weekend];
+console.log(weekdays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+// Reatribuindo valores de objeto através do Object Destructuring
+const student = {
+    a: 'Maria',
+    b: 'Turma B',
+    c: 'Matematica',
+};
+
+const { a: name, b: classAssigned, c: subject } = student;
+
+console.log(name); // Maria
+console.log(classAssigned); // Turma B
+console.log(subject); // Matemática
